687c26dc7619b0d1bb8fa72b837911318d3aab62
d1zm4as <pedrofamilia364@gmail.com>
2024-02-25 20:48:45 -0300

    +!

---
+++ problem2.py
@@ -0,0 +1,10 @@
+def even_fib(m):
+    x,y = 0, 1
+    counter = 0
+    while y <m:
+        if y % 2 == 0:
+            counter += y
+        x,y = y, x+ y
+    return counter
+
+print(even_fib(4_000_000))
